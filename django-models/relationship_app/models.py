2cf373dc888fd9879c94874549783dac82f7b677
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-22 12:06:04 +0200

    first changes in relationship_app

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+# Author model
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+# Library model
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name 
+
+# Librarian model
+class Librarian(models.Model):
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
