c84168cf606515cfe4cef1398c64775138648736
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-22 12:26:04 +0200

    query_samples created

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
-from .models import Book, Library, Librarian
+from .models import Book, Library, Librarian, Author
 # Queries for the database
 
 # Query all books by a specific author
-Book.objects.filter(author='Joe different')
-
+author = Author.objects.get(author='Joe different')
+Book.objects.filter(author= author)
 # List all books in a library.
-library= Library.objects.get(name=library_name)
-library.books.all()
+library_name= Library.objects.get(name=library_name)
+library_name.books.all()
 # Retrieve the librarian for a library.
-Librarian.objects.get(library= 'Saint Lorence')
+Librarian.objects.get(library= library_name)
