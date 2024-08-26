f154d2cc4c955f6f2bc56f55d74ff4eb0aaa1a98
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-26 21:23:25 +0200

    admin file updated

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,6 @@
 
-from django.contrib import admin
 from  .models import Book
+from django.contrib import admin
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display =('title', 'author', 'publication_year')
