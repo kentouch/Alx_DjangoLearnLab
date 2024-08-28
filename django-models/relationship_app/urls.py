c07dfdb81a9931ca4600693b057fc03890bfdf8b
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-28 10:46:44 +0200

    user auth updated in views and urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.list_books, name='home'),
     path('register/', views.register, name='register' ),
     path('library/<int:pk>/', LibraryDetailViews.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name = 'registration/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name= 'registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= 'registration/login.html'), name='logout')
 ]
