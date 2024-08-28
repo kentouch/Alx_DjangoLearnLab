e9621cff3671a6a19b5032759fd9324f7e96804d
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-28 10:37:25 +0200

    user auth updated in views and urls

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailViews
+from . import views
+from .views import LibraryDetailViews
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', list_books, name='list_books'),
+    path('', views.list_books, name='home'),
+    path('register/', views.register, name='register' ),
     path('library/<int:pk>/', LibraryDetailViews.as_view(), name='library_detail'),
-    path('/login', LoginView.as_view(template_name = 'registration/login.html'), name='login'),
-    path('/logout', LogoutView.as_view(), name='logout')
+    path('login/', LoginView.as_view(template_name = 'registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name= 'registration/login.html'), name='logout')
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,28 +1,35 @@
 from typing import Any
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView, CreateView
 from .models import Library, Author, Librarian, Book
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib import messages
+
 
 # Create your views here.
 
-class SignupView(CreateView):
-    # uses the usercreationform as a form base for registration
-    forms = UserCreationForm
-    # After successful registration, there is a redirection to the login page
-    success_url = reverse_lazy('login')
-    # template name for the the registration form
-    template_name = 'registr_templates/signup.html'
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm
+        if form.is_valid:
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            return messages.error('registration failed, try again')
+    else:
+        form = UserCreationForm
+        return render(request, 'registration/signup.html', {'form': form})
+
 
 #Implement Function-based View
 def list_books(request):
     # retrieve all the books 
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'relationship_app/list_books.html', context)
+    return render(request, 'relationship_app/home.html', context)
 
 #Implement Class-based View:
 class LibraryDetailViews(DetailView):
