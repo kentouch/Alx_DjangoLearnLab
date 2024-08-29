5d4fa8df1d3bc43fe757bcbd653f16df9acb78d8
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-29 16:12:26 +0200

    user authentication upadted 2nd tim

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,16 +2,16 @@
 
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from . import views
 from . import admin_view, librarian_view, member_view
 
 urlpatterns = [
 
-    path('', list_books, name='list_books'),
-    path('register/', register, name='register'),
+    path('', views.list_books, name='list_books'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name='logout'),
-    path('library/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('admin/', admin_view.admin_only_view, name='admin_view'),
     path('librarian/', librarian_view.librarian_only_view, name='librarian_view'),
     path('member/', member_view.member_only_view, name='member_view'),
