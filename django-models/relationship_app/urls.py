ea752938c44c4766f9090a95a110b15d0d108894
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-29 14:56:00 +0200

    views updated 6th tim

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,11 @@
 # urls.py
 
 from django.urls import path
-from views import LibraryView
-from . import admin_view, librarian_view, member_view, views
+from .views import list_books, LibraryView
+from . import admin_view, librarian_view, member_view
 
 urlpatterns = [
-    path('', views.list_books, name='list_books'),
+    path('', list_books, name='list_books'),
     path('library/', LibraryView.as_view(), name='library_detail'),
     path('admin/', admin_view.admin_only_view, name='admin_view'),
     path('librarian/', librarian_view.librarian_only_view, name='librarian_view'),
