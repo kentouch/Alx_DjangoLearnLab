3d63b1d551e2c87f77202e51da6c53f2182e6184
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-30 10:42:10 +0200

    urls file updated

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,8 +7,8 @@ from . import admin_view, librarian_view, member_view
 
 urlpatterns = [
 
-    path('', views.list_books, name='list_books'),
     path('register/', views.register, name='register'),
+    path('', views.list_books, name='list_books'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name='logout'),
     path('library/', views.LibraryDetailView.as_view(), name='library_detail'),
