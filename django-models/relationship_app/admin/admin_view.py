b89577cb42ec1e4543edd2952dba9fbe224b0058
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-30 14:02:09 +0200

    admin view updated

--- django-models/relationship_app/admin/admin_view.py
+++ django-models/relationship_app/admin/admin_view.py
@@ -1,6 +1,9 @@
-from .test_func import is_admin
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
 
 @user_passes_test(is_admin)
 def admin_only_view(request):
