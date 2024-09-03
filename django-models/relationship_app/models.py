f8545189be91cfa6f86c6a9b7962983ad951027d
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-03 10:56:20 +0200

    Meta class added to the book model

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -32,6 +32,13 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
 # Library model
 class Library(models.Model):
     name = models.CharField(max_length=200)
