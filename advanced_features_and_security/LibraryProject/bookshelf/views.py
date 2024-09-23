d9fbbc92adf7368e64e7b22498e435c508e75e6d
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-23 14:33:22 +0200

    views file updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # create views here
 
-
+# list_of_books views
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/home.html', {'list_books':books})
 
