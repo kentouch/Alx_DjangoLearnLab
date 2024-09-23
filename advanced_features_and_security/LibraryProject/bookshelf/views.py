1770bbc75aed6841577d8fc797475503d7ad270a
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-23 14:36:18 +0200

    models.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth.decorators import permission_required
 
 # create views here
 
 # list_of_books views
+# let's create a view and add a permission decorator for restriction to the list of books
+@permission_required('bookshelf.can_view')
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/home.html', {'list_books':books})
