d8520b646d2f052bbeba85d7cd008fe429f1b577
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-25 15:50:05 +0200

    permissions and authentication implemented

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path, include
 from .views import BookViewSet, BookList
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -14,5 +15,6 @@ urlpatterns = [
     path('api/books/<int:pk>/update/', BookViewSet.as_view({'put': 'update'}), name= 'BookUpdateView'),
     path('api/books/<int:pk>/delete/', BookViewSet.as_view({'delete': 'destroy'}), name= 'BookDeleteView'),
     path('api/books/create/', BookViewSet.as_view({'post': 'create'}), name= 'BookCreateView'),
+    path('api/auth/', obtain_auth_token, name= 'AuthToken'),
     path('api/', include(router.urls))
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from api.serializers import BookSerializer
 from rest_framework import generics, viewsets
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 class BookList(generics.ListCreateAPIView):
@@ -9,5 +10,8 @@ class BookList(generics.ListCreateAPIView):
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    permissions = [IsAdminUser]
+    # queryset = Book.objects.all()
     queryset = Book.objects.all()
     serializer_class = BookSerializer
