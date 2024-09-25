aa1f04cacbb615426e76c130dffb8e846e562904
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-25 15:55:50 +0200

    viewsets and routers implemented in the app

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -42,6 +42,13 @@ INSTALLED_APPS = [
     'api'
 ]
 
+# Default authentication classes
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
