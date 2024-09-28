df49ebd93ab57060411613057901f99a210530a8
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-28 11:39:01 +0200

    views.py updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author
+from serializers import BookSerializer
 
 # Create your views here.
+# This view is used to retrieve all books in the database
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Construct a view for creating books through the API
+class BookCreateApiView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+# Construct a view for updating books through the API
+"""class BookUpdateApiView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_update(self, serializer):
+        serializer.save()"""
+
+# Construct a view for detailing books through the API
+
+class BookDetailApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
