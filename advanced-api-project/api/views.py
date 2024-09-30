a3156ae35684cddc2a5e817eafb727c8fe6c4965
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-30 11:59:07 +0200

    Implemented urls path for each view

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
 from serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # Create your views here.
 # This view is used to retrieve all books in the database
@@ -21,18 +21,18 @@ class BookDetailView(generics.DetailApiView):
 # A DetailView for retrieving a single book by ID.
 class BookCreateView(generics.CreateApiView):
     # permissions = [IsAuthenticatedOrReadOnly, IsAdminUser]
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # A CreateView for adding a new book.
 class BookUpdateView(generics.UpdateApiView):
-    permissions = [IsAdminUser]
+    permissions = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # An UpdateView for modifying an existing book.
 class BookDeleteView(generics.DeleteApiView):
-    permissions = [IsAdminUser]
+    permissions = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # A DeleteView for removing a book.
