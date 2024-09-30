24c445fe4aefba61dcd29e47900943d8d956df3c
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-30 11:07:19 +0200

    Implemented urls path for each view

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # this path is used for the GET request for all books
     path('books/', views.BookListView.as_view()),
+    # This path is used for the GET request for a single book
     path('books/<int:pk>/', views.BookDetailView.as_view()),
+    # These three paths are used for the POST, PUT, and DELETE requests for books
     path('books/create/', views.BookCreateView.as_view()),
-    path('books/<int:pk>/update/', views.BookUpdateView.as_view()),
-    path('books/<int:pk>/delete/', views.BookDeleteView.as_view()),
+    path('books/update/', views.BookUpdateView.as_view()),
+    path('books/delete/', views.BookDeleteView.as_view()),
 ]
