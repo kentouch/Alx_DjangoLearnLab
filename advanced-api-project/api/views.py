0cb3a10f96d343aba7cdf9635bd510cb991279d0
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-09-30 11:57:37 +0200

    Implemented permissions checks in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
 from serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 
 # Create your views here.
 # This view is used to retrieve all books in the database
 class BookListView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
@@ -13,18 +15,24 @@ class BookListView(generics.ListCreateAPIView):
 # Implement a set of generic views for the Book model to handle CRUD operations. This includes:
 # A ListView for retrieving all books.
 class BookDetailView(generics.DetailApiView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # A DetailView for retrieving a single book by ID.
 class BookCreateView(generics.CreateApiView):
+    # permissions = [IsAuthenticatedOrReadOnly, IsAdminUser]
+    permission_classes = [IsAdminUser]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # A CreateView for adding a new book.
 class BookUpdateView(generics.UpdateApiView):
+    permissions = [IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # An UpdateView for modifying an existing book.
 class BookDeleteView(generics.DeleteApiView):
+    permissions = [IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 # A DeleteView for removing a book.
