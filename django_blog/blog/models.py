4cebc0af70b1ab0683df508d5321a603f7ce0dd9
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-10-03 23:18:13 +0200

    Finished setting up my app and settings for the project

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
