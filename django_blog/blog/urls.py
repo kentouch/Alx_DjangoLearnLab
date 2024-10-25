e6db589d1b2f5c5557966ec3151114f4c5eeb947
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-10-25 07:57:13 +0200

    blog urls updated

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('logout', views.HomeView.logout, name='logout'),
     path('register/', views.HomeView.register, name='register'),
     path('profile/', views.HomeView.profile, name='profile'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-updated'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
