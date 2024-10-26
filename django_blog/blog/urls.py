8114c94fab500767c44dcf1b655a11a5f4afcb50
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-10-26 14:08:02 +0200

    comment urls implemented

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,8 +9,13 @@ urlpatterns = [
     path('logout', views.HomeView.logout, name='logout'),
     path('register/', views.HomeView.register, name='register'),
     path('profile/', views.HomeView.profile, name='profile'),
+    # Post urls
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-updated'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    # Comment urls
+    path('comment/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update', views.CommentUpdateView.as_view(), name='comment-updated'),
+    path('comment/<int:pk>/delete', views.CommentUpdateView.as_view(), name='comment-delete')
 ]
